08c941b81476b7afb30ea3eb289ef86fb22f96b9
serjant007 <greatserjant007@gmail.com>
2020-10-21 13:18:43 +0700

    add notification with help alert

--- src/App.js
+++ src/App.js
@@ -4,99 +4,109 @@ import axios from 'axios';
 import { Button, Create, Edit, Header, Person } from './Components';
 
 function App() {
-    const [list, setList] = React.useState(null);
-    const [editUser, setEditUser] = React.useState({});
+  const [list, setList] = React.useState(null);
+  const [editUser, setEditUser] = React.useState({});
 
-    const onAddPerson = (obj) => {
-        const newPerson = [...list, obj];
-        setList(newPerson);
-    };
+  const onAddPerson = (obj) => {
+    const newPerson = [...list, obj];
+    setList(newPerson);
+  };
 
-    const onEditPerson = (obj) => {
-        const newList = list.map((el) => {
-            if (el.id === obj.id) {
-                return obj;
-            }
-            return el;
-        });
-        setList(newList);
-        axios.patch('http://localhost:3001/persons/' + obj.id, {
-            id: obj.id,
-            firstName: obj.firstName,
-            lastName: obj.lastName,
-        });
-    };
-
-    const editPersone = (item) => {
-        setEditUser(item);
-    };
-
-    React.useEffect(() => {
-        axios.get('http://localhost:3001/persons').then(({ data }) => {
-            setList(data);
-        });
-    }, []);
-
-    const onRemove = (item, id) => {
-        if (
-            window.confirm(` Вы действительно хотите удалить ${item.firstName} ${item.lastName}?`)
-        ) {
-            axios.delete('http://localhost:3001/persons/' + item.id).then(() => {
-                const listUpd = [...list];
-                listUpd.splice(id, 1);
-                setList(listUpd);
-            });
+  const onEditPerson = (obj) => {
+    const newList = list.map((el) => {
+      if (el.id === obj.id) {
+        return obj;
+      }
+      return el;
+    });
+    setList(newList);
+    axios
+      .patch('http://localhost:3001/persons/' + obj.id, {
+        id: obj.id,
+        firstName: obj.firstName,
+        lastName: obj.lastName,
+      })
+      .then(function (response) {
+        if (response.status === 200) {
+          alert('Успешно изменено');
         }
-    };
+      })
+      .catch(function (error) {
+        alert('Нет соединения с БД');
+      });
+  };
 
-    const [showCreatePop, setShowCreatePop] = React.useState(false);
-    const [showEditPop, setShowEditPop] = React.useState(false);
+  const editPersone = (item) => {
+    setEditUser(item);
+  };
 
-    const showCreate = () => {
-        setShowCreatePop(!showCreatePop);
-    };
-    const closeCreate = () => {
-        setShowCreatePop(false);
-    };
+  React.useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(({ data }) => {
+        setList(data);
+      })
+      .catch(function (error) {
+        alert('Нет соединения с БД');
+      });
+  }, []);
 
-    const showEdit = () => {
-        setShowEditPop(!showEditPop);
-    };
-    const closeEdit = () => {
-        setShowEditPop(false);
-    };
+  const onRemove = (item, id) => {
+    if (window.confirm(` Вы действительно хотите удалить ${item.firstName} ${item.lastName}?`)) {
+      axios
+        .delete('http://localhost:3001/persons/' + item.id)
+        .then(() => {
+          const listUpd = [...list];
+          listUpd.splice(id, 1);
+          setList(listUpd);
+        })
+        .then(() => {
+          alert('Успешно удалено');
+        });
+    }
+  };
 
-    return (
-        <div className="App">
-            <div className="container">
-                <Header />
-                {list ? (
-                    <Person
-                        showEdit={showEdit}
-                        items={list}
-                        onRemove={onRemove}
-                        editItem={editPersone}
-                    />
-                ) : (
-                    <h4>Загрузка данных...</h4>
-                )}
-                <Button text="Добавить сотрудника" onCreate={showCreate} />
-            </div>
-            {showCreatePop && (
-                <Create list={list} onAdd={onAddPerson} closeCreatePop={closeCreate} />
-            )}
-            {showEditPop && (
-                <Edit
-                    list={list}
-                    onAdd={onEditPerson}
-                    closeEditPop={closeEdit}
-                    editItem={showEdit}
-                    editPersonItem={editPersone}
-                    editData={editUser}
-                />
-            )}
-        </div>
-    );
+  const [showCreatePop, setShowCreatePop] = React.useState(false);
+  const [showEditPop, setShowEditPop] = React.useState(false);
+
+  const showCreate = () => {
+    setShowCreatePop(!showCreatePop);
+  };
+  const closeCreate = () => {
+    setShowCreatePop(false);
+  };
+
+  const showEdit = () => {
+    setShowEditPop(!showEditPop);
+  };
+  const closeEdit = () => {
+    setShowEditPop(false);
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <Header />
+        {list ? (
+          <Person showEdit={showEdit} items={list} onRemove={onRemove} editItem={editPersone} />
+        ) : (
+          <h4>Загрузка данных...</h4>
+        )}
+        <Button text="Добавить сотрудника" onCreate={showCreate} />
+      </div>
+      {showCreatePop && <Create list={list} onAdd={onAddPerson} closeCreatePop={closeCreate} />}
+      {showEditPop && (
+        <Edit
+          list={list}
+          onAdd={onEditPerson}
+          closeEditPop={closeEdit}
+          editItem={showEdit}
+          editPersonItem={editPersone}
+          editData={editUser}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Create.js
+++ src/Components/Create.js
@@ -3,64 +3,67 @@ import React from 'react';
 import ButtonSave from './ButtonSave';
 
 function Create({ closeCreatePop, onAdd }) {
-    const [inputName, setInputName] = React.useState('');
-    const [inputSurname, setInputSurname] = React.useState('');
+  const [inputName, setInputName] = React.useState('');
+  const [inputSurname, setInputSurname] = React.useState('');
 
-    const addPerson = () => {
-        if (!inputName) {
-            alert('Введите имя сотрудника');
-            return;
-        } else if (!inputSurname) {
-            alert('Введите фамилию сотрудника');
-            return;
-        }
-        axios
-            .post('http://localhost:3001/persons', { firstName: inputName, lastName: inputSurname })
-            .then(({ data }) => {
-                onAdd(data);
-            });
-        closeCreatePop();
-    };
-    return (
-        <div className="overlay">
-            <div className="create">
-                <div className="create__form-block">
-                    <div className="create__header">
-                        <h3 className="create__form-title">Создание сотрудника</h3>
-                    </div>
-                    <div className="wrapper">
-                        <div className="create__back">
-                            <button onClick={closeCreatePop}>Назад к списку</button>
-                        </div>
-                        <div className="create__form">
-                            <input
-                                value={inputName}
-                                onChange={(e) => setInputName(e.target.value)}
-                                className="contacts__form-input"
-                                type="text"
-                                name="name"
-                                placeholder="Введите имя сотрудника"
-                            />
-                            <input
-                                value={inputSurname}
-                                onChange={(e) => setInputSurname(e.target.value)}
-                                className="contacts__form-input"
-                                type="text"
-                                name="surname"
-                                placeholder="Введите фамилию сотрудника"
-                            />
-                        </div>
-                        <ButtonSave
-                            onCloseCreate={closeCreatePop}
-                            onClick={addPerson}
-                            text="Сохранить"
-                            pos="create__btn-save"
-                        />
-                    </div>
-                </div>
+  const addPerson = () => {
+    if (!inputName) {
+      alert('Введите имя сотрудника');
+      return;
+    } else if (!inputSurname) {
+      alert('Введите фамилию сотрудника');
+      return;
+    }
+    axios
+      .post('http://localhost:3001/persons', { firstName: inputName, lastName: inputSurname })
+      .then(({ data }) => {
+        onAdd(data);
+      })
+      .then(() => {
+        alert('Успешно добавлено');
+      });
+    closeCreatePop();
+  };
+  return (
+    <div className="overlay">
+      <div className="create">
+        <div className="create__form-block">
+          <div className="create__header">
+            <h3 className="create__form-title">Создание сотрудника</h3>
+          </div>
+          <div className="wrapper">
+            <div className="create__back">
+              <button onClick={closeCreatePop}>Назад к списку</button>
             </div>
+            <div className="create__form">
+              <input
+                value={inputName}
+                onChange={(e) => setInputName(e.target.value)}
+                className="contacts__form-input"
+                type="text"
+                name="name"
+                placeholder="Введите имя сотрудника"
+              />
+              <input
+                value={inputSurname}
+                onChange={(e) => setInputSurname(e.target.value)}
+                className="contacts__form-input"
+                type="text"
+                name="surname"
+                placeholder="Введите фамилию сотрудника"
+              />
+            </div>
+            <ButtonSave
+              onCloseCreate={closeCreatePop}
+              onClick={addPerson}
+              text="Сохранить"
+              pos="create__btn-save"
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Create;
